f5074e6463ee872abd7b94992c2962d8d958403e
GasparDing <kai21413@gmail.com>
2019-05-15 22:12:52 +0800

    nothign

--- DcardCrawler.Test/CrawlerTest.cs
+++ DcardCrawler.Test/CrawlerTest.cs
@@ -10,14 +10,17 @@ namespace DcardCrawler.Test
     [TestFixture]
     public class CrawlerTest
     {
+
+        string Id = "231268139";
         public CrawlerTest()
         {
+            
         }
 
         [Test]
-        public void DcardCrawlerTest()
+        public void ListCrawlerTest()
         {
-            HttpWebRequest request = WebRequest.Create("https://www.dcard.tw/f/sex") as HttpWebRequest;
+            HttpWebRequest request = WebRequest.Create("https://www.dcard.tw/_api/forums/sex/posts?popular=true&limit=30") as HttpWebRequest;
             var response = request.GetResponse();
             string responseString = null;
             using (var stream = response.GetResponseStream())
@@ -26,5 +29,34 @@ namespace DcardCrawler.Test
                     responseString = streamReader.ReadToEnd();
             }
         }
+
+        [Test]
+        public void PostCrawlerTest()
+        {
+            HttpWebRequest request = WebRequest.Create("https://www.dcard.tw/_api/posts/231268139") as HttpWebRequest;
+            var response = request.GetResponse();
+            string responseString = null;
+            using (var stream = response.GetResponseStream())
+            {
+                using (var streamReader = new StreamReader(stream))
+                    responseString = streamReader.ReadToEnd();
+            }
+        }
+
+         [Test]
+        public void CommentCrawlerTest()
+        {
+            // todo: 撈出來的資料跟網站上的資料不一樣, 似乎是沒登入有快取的問題
+            HttpWebRequest request = WebRequest.Create("https://www.dcard.tw/_api/posts/231268139/comments?limit=50") as HttpWebRequest;
+            var response = request.GetResponse();
+            string responseString = null;
+            using (var stream = response.GetResponseStream())
+            {
+                using (var streamReader = new StreamReader(stream))
+                    responseString = streamReader.ReadToEnd();
+            }
+        }
+
+
     }
 }
