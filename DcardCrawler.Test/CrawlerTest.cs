d7dc5638505541edbf5b48c8db38c978aeb2a25b
GasparDing <kai21413@gmail.com>
2019-05-15 18:43:19 +0800

    entity

--- DcardCrawler.Test/CrawlerTest.cs
+++ DcardCrawler.Test/CrawlerTest.cs
@@ -17,8 +17,34 @@ namespace DcardCrawler.Test
         [Test]
         public void DcardCrawlerTest()
         {
-            HttpWebRequest request = WebRequest.Create("https://www.dcard.tw/f/sex") as HttpWebRequest;
-            var response = request.GetResponse();
+            //HttpWebRequest request = WebRequest.Create("https://www.dcard.tw/f/sex") as HttpWebRequest;
+            HttpWebRequest request = WebRequest.Create("https://www.dcard.tw/_api/posts/231223641") as HttpWebRequest;
+            WebResponse response = null;
+
+            try
+            {
+                response = request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                if (e.Status == WebExceptionStatus.ProtocolError && e.Response != null)
+                {
+                    var resp = (HttpWebResponse)e.Response;
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        // Do something
+                    }
+                    else
+                    {
+                        // Do something else
+                    }
+                }
+                else
+                {
+                    // Do something else
+                }
+            }
+
             string responseString = null;
             using (var stream = response.GetResponseStream())
             {
